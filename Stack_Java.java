e5392bae46bffec0f80de3891d40fd9c22582538
kanchan-solanke <kanchanssolanke@gmail.com>
2021-07-26 23:46:17 +0530

    Ability to peak and

--- Stack_Java.java
+++ Stack_Java.java
@@ -1,24 +1,23 @@
 import static java.lang.System.exit;
 
-class Stack_JavaUC1 {
-    class Node {
+ class Stack_JavaUC1 {
+    class Node{
         int data;
         Node link;
 
         public Node(int x) {
         }
     }
-
     Node top;       //global variable reference for top of stack
 
     // Constructor
 
-    Stack_JavaUC1() {
+    Stack_JavaUC1(){
         this.top = null;  //set null to top for empty stack
     }
 
     //to add elements in stack
-    public void push(int x) {
+    public void push(int x){
         // create new node temp and allocate memory
         Node temp = new Node(x);
 
@@ -38,31 +37,73 @@ class Stack_JavaUC1 {
         // update top reference
         top = temp;
     }
-
-    public void display() {
+    public void display()
+    {
         // check for stack underflow
         if (top == null) {
             System.out.printf("Stack Underflow");
             exit(1);
-        } else {
+        }
+        else {
             Node temp = top;
             while (temp != null) {
 
                 // print node data
-                System.out.printf("%d->", temp.data);
+                System.out.printf("%d->",temp.data);
 
                 // assign temp link to temp
                 temp = temp.link;
             }
         }
     }
+    // Utility function to return top element in a stack
+    public int peek()
+    {
+        // check for empty stack
+        if (!isEmpty()) {
+            return top.data;
+        }
+        else {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+    }
 
-    public static void main(String[] args) {
+    private boolean isEmpty() {
+        return false;
+    }
+
+    // Utility function to pop top element from the stack
+    public void pop(int i) // remove at the beginning
+    {
+        // check for stack underflow
+        if (top == null) {
+            System.out.print("\nStack Underflow");
+            return;
+        }
+
+        // update the top pointer to point to the next node
+        top = (top).link;
+    }
+
+
+    public static void main(String[] args){
         Stack_JavaUC1 obj = new Stack_JavaUC1();
         obj.push(70);
         obj.push(30);
         obj.push(56);
-        // print Stack elements
+    // print Stack elements
+    obj.display();
+        // print Top element of Stack
+        System.out.printf("Top element is %d", obj.peek());
+
+        // Delete top element of Stack
+        obj.pop(56);
+        obj.pop(30);
+        obj.pop(70);
+
         obj.display();
+
     }
+
 }
